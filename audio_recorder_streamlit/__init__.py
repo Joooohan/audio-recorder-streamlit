20647eda0d5aca5cf77cf827e8bf2d1af1105d58
Johan Leduc <johan.leduc90@gmail.com>
2023-01-02 10:33:21 +0100

    v0.0.5

--- audio_recorder_streamlit/__init__.py
+++ audio_recorder_streamlit/__init__.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 import streamlit.components.v1 as components
 
-_RELEASE = False
+_RELEASE = True
 
 if not _RELEASE:
     _audio_recorder = components.declare_component(
