f9ecc667a24785b70599fbd0df89bdb190d18be3
Johan Leduc <johan.leduc90@gmail.com>
2023-01-02 11:40:26 +0100

    Add sanity test before building

--- audio_recorder_streamlit/__init__.py
+++ audio_recorder_streamlit/__init__.py
@@ -6,16 +6,16 @@ import streamlit.components.v1 as components
 
 _RELEASE = True
 
-if not _RELEASE:
-    _audio_recorder = components.declare_component(
-        "audio_recorder",
-        url="http://localhost:3001",
-    )
-else:
+if _RELEASE:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
     _audio_recorder = components.declare_component(
         "audio_recorder", path=build_dir
+    )
+else:
+    _audio_recorder = components.declare_component(
+        "audio_recorder",
+        url="http://localhost:3001",
     )
 
 
---
+++ tests/test_sanity.py
@@ -0,0 +1,3 @@
+from audio_recorder_streamlit import _RELEASE
+
+assert _RELEASE
